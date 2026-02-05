bbdf39f03cdccc5e17847c9d0b5dae1614184895
Tasuku Yamashita <task.teckac@gmail.com>
2026-02-05 09:32:36 +0900

    ci: pin gion-core ref for dependency checkout

--- .github/scripts/setup-gion-core-dependency.sh
+++ .github/scripts/setup-gion-core-dependency.sh
@@ -9,3 +9,8 @@ repo="${GION_CORE_REPO:-https://github.com/tasuku43/gion-core.git}"
 ref="${GION_CORE_REF:-main}"
 
 git clone --depth=1 --branch "${ref}" "${repo}" ../gion-core
+
+if [[ ! -f "../gion-core/go.mod" ]]; then
+  echo "gion-core dependency is missing go.mod (repo=${repo}, ref=${ref})" >&2
+  exit 1
+fi
